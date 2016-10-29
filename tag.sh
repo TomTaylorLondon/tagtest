da2a8e0a1f4a0069809ae8cb6212127b61349283
Tom Taylor <thomastaylor.uk@gmail.com>
2016-10-29 18:42:03 +0100

    Updating tag

--- tag.sh
+++ tag.sh
@@ -1,4 +1,42 @@
 #!/bin/bash
 
-VERSION=1.144.0
-git tag -a $VERSION
+# http://stackoverflow.com/questions/3878624/how-do-i-programmatically-determine-if-there-are-uncommited-changes
+require_clean_work_tree () {
+    # Update the index
+    git update-index -q --ignore-submodules --refresh
+    err=0
+
+    # Disallow unstaged changes in the working tree
+    if ! git diff-files --quiet --ignore-submodules --
+    then
+        echo >&2 "cannot $1: you have unstaged changes."
+        git diff-files --name-status -r --ignore-submodules -- >&2
+        err=1
+    fi
+
+    # Disallow uncommitted changes in the index
+    if ! git diff-index --cached --quiet HEAD --ignore-submodules --
+    then
+        echo >&2 "cannot $1: your index contains uncommitted changes."
+        git diff-index --cached --name-status -r --ignore-submodules HEAD -- >&2
+        err=1
+    fi
+
+    if [ $err = 1 ]
+    then
+        echo >&2 "Please commit or stash them."
+        exit 1
+    fi
+}
+
+
+VERSION=$(cat version)
+echo "checking workspace"
+require_clean_work_tree
+echo "creating tag $VERSION"
+git tag -a $VERSION -m $VERSION
+if [ $? -ne 0 ]; then
+    exit $?
+fi
+echo "pushing tag"
+git push origin $VERSION
